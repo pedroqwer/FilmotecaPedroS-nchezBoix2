5181ac5bd503da3569ea4c78a804fb5fe37e0a76
pedroqwer <116349992+pedroqwer@users.noreply.github.com>
2023-12-29 13:25:31 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/proyecto/MainActivity.java
+++ app/src/main/java/com/example/proyecto/MainActivity.java
@@ -14,9 +14,13 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
-    private ListView lista;
-    private     FilmListActivity filmListActivity;
+  private ListView lista;
+
+    private static int DATA_FILM =1;
+    FilmDataSource filmDataSource;
+    FilmListActivity filmListActivity;
     Film film;
+    FilmListActivity filmAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +28,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         FilmDataSource.Initialize();
         lista=(ListView) findViewById(R.id.listaa);
-        filmListActivity=new FilmListActivity(this,R.layout.mostrar, FilmDataSource.films);
+        filmListActivity=new FilmListActivity(this,R.layout.mostrar,FilmDataSource.films);
+
         lista.setAdapter(filmListActivity);
         lista.setOnItemClickListener(this);
 
@@ -32,41 +37,41 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
 
     }
+
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        // Acción al clickar sobre un elemento determinado
-        Intent intent = new Intent(MainActivity.this, FilmDataActivity.class);
-        intent.putExtra("FILM_POSITION", position);
-        Toast.makeText(getApplicationContext(),"Película selecionada : "+FilmDataSource.films.get(position).getTitle(), Toast.LENGTH_LONG).show();
-        if (intent.resolveActivity(getPackageManager()) != null){
-            startActivity(intent);
-        }
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Intent intent=new Intent(MainActivity.this,FilmDataActivity.class);
+        intent.putExtra("Pelicula",i);
+        Toast.makeText(getApplicationContext(),"Película selecionada : "+FilmDataSource.films.get(i).getTitle(), Toast.LENGTH_LONG).show();
+        startActivity(intent);
 
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu,menu);
-        return  true;
+        return true;
     }
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         getMenuInflater().inflate(R.menu.menucon,menu);
-
         super.onCreateContextMenu(menu, v, menuInfo);
     }
 
     @Override
-    public boolean onOptionsItemSelected (@NonNull MenuItem item){
-
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id=item.getItemId();
 
-        if(id==R.id.opcion1){
+        /* if(id==R.id.opcion1){
             Toast.makeText(getApplicationContext(), "Has pulsado Acerca de ", Toast.LENGTH_LONG).show();
-            Intent si=new Intent(this,AboutActivity.class);
-            startActivity(si);
+           Intent si=new Intent(this,AboutActivity.class);
+            startActivity(si);*/
 
+        if(id==R.id.abo){
+            Toast.makeText(getApplicationContext(), "Has pulsado Acerca de ", Toast.LENGTH_LONG).show();
+            Intent in =new Intent(this, AbouTActivityy.class);
+            startActivity(in);
         }else if (id==R.id.opcion2){
             Toast.makeText(getApplicationContext(),"Has pulsado añadir peliculas",Toast.LENGTH_LONG).show();
             FilmDataSource.films.add(film=new Film(R.drawable.ic_launcher_background,"Titulo nuevo","Director nuevo",2002,1,2,"Nuevo enlace","Nuevo comentario"));
@@ -75,25 +80,50 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             //startActivity(si);
         }
 
-        return  super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected(item);
     }
+
 
     @Override
     public boolean onContextItemSelected(@NonNull MenuItem item) {
-
         AdapterView.AdapterContextMenuInfo info=(AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
         Film f=(Film) lista.getItemAtPosition(info.position);
 
         int id=item.getItemId();
 
         if(id==R.id.eliminar){
-            Toast.makeText(getApplicationContext(), "Eliminar " + f.getTitle(), Toast.LENGTH_LONG).show();
-            FilmDataSource.films.remove(f);
 
-            filmListActivity.notifyDataSetChanged();
+            AlertDialog.Builder alert=new AlertDialog.Builder(MainActivity.this);
+            alert.setMessage("Desea borrar la película seleccionada?")
+                    .setCancelable(false)
+                    .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Toast.makeText(getApplicationContext(), "Eliminar " + f.getTitle(), Toast.LENGTH_LONG).show();
+                            FilmDataSource.films.remove(f);
+                            filmListActivity.notifyDataSetChanged();
+                        }
+                    }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                            Toast.makeText(getApplicationContext(),"Película no eliminada",Toast.LENGTH_LONG).show();
+                        }
+                    });
+            AlertDialog aa=alert.create();
+            aa.setTitle("Borrar");
+            aa.show();
 
         }
-
         return super.onContextItemSelected(item);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == DATA_FILM){
+            if (resultCode == RESULT_OK){
+                filmAdapter.notifyDataSetChanged();
+            }
+        }
     }
 }
