60df46733f45faff23bfd0dc6ee58062f9504c3a
pedroqwer <116349992+pedroqwer@users.noreply.github.com>
2023-12-29 13:27:01 +0100

    Update AboutActivity.java

--- app/src/main/java/com/example/proyecto/AboutActivity.java
+++ app/src/main/java/com/example/proyecto/AboutActivity.java
@@ -10,53 +10,45 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class AboutActivity extends AppCompatActivity {
-    Button fin;
+    Button button,buttonS,fin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
-        //Boton 1
-        Button button1 = findViewById(R.id.boton1);
-        button1.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_abou_tactivityy);
+        button=(Button) findViewById(R.id.boton1);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                String url = "https://github.com/pedroqwer/FilmotecaPedroS-nchezBoix2";
-
-                Intent intent = new Intent(Intent.ACTION_VIEW);
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Enlace de github", Toast.LENGTH_SHORT).show();
+                String url="https://github.com/pedroqwer/FilmotecaPedroS-nchezBoix2";
+                Intent intent=new Intent(Intent.ACTION_VIEW);
                 intent.setData(Uri.parse(url));
 
                 startActivity(intent);
-
-
             }
         });
-
-        //Boton 2
-        Button button2 = findViewById(R.id.boton2);
-        button2.setOnClickListener(new View.OnClickListener() {
+        buttonS=(Button) findViewById(R.id.boton2);
+        buttonS.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-
-                Intent intent = new Intent(Intent.ACTION_SEND);
-
-                intent.setType("Text/plain");
-                intent.putExtra(Intent.EXTRA_SUBJECT, "Soporte Filmoteca");
-                intent.putExtra(Intent.EXTRA_TEXT, "Texto del correo de soporte");
-                intent.putExtra(Intent.EXTRA_EMAIL, new String[] {"filmoteca@pmdm.es"});
+            public void onClick(View view) {
+                Intent intent=new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_SUBJECT,"Soporte Filmoteca");
+                intent.putExtra(Intent.EXTRA_TEXT,"Texto del correo de soporte");
+                intent.putExtra(Intent.EXTRA_EMAIL,new String[]{"filmoteca@pmdm.es"});
 
                 startActivity(intent);
-
             }
         });
-        //boton 3
 
         fin=(Button) findViewById(R.id.boton3);
         fin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(AboutActivity.this,MainActivity.class);
+                Intent intent = new Intent(AbouTActivityy.this, MainActivity.class);
                 startActivity(intent);
-                Toast.makeText(getApplicationContext(),"Has vuelto al principio",Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Has vuelto al principio", Toast.LENGTH_LONG).show();
             }
         });
 
