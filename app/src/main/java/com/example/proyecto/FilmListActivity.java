b05ec29e00b8d5c7ae11754214224d226b5d5d9e
pedroqwer <116349992+pedroqwer@users.noreply.github.com>
2024-02-16 16:48:30 +0100

    Update FilmListActivity.java

--- app/src/main/java/com/example/proyecto/FilmListActivity.java
+++ app/src/main/java/com/example/proyecto/FilmListActivity.java
@@ -1,35 +1,35 @@
 package com.example.proyecto;
 
 import android.content.Context;
-import android.content.Intent;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 
-public class FilmListActivity extends ArrayAdapter<Film>  {
-    private  int rr;
-    private ArrayList<Film> films;
+public class FilmListActivity extends ArrayAdapter<Film> {
+
+    int rr;
+
+    ArrayList<Film> films;
     public FilmListActivity(@NonNull Context context, int resource, @NonNull ArrayList<Film> objects) {
         super(context, resource, objects);
+
         rr=resource;
         films=objects;
-    }
 
+    }
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
         LayoutInflater inflater=LayoutInflater.from(getContext());
         View mi=inflater.inflate(rr,parent,false);
 
